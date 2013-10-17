33c556bc89bfd7c09e80d8b6dbc3932f60eed2f0
Fernando Blat <ferblape@gmail.com>
2013-10-17 20:42:06 +0200

    NoMasDeMama adapter returns Markdown

--- app/models/recipe_fetcher/adapters/no_mas_de_mama.rb
+++ app/models/recipe_fetcher/adapters/no_mas_de_mama.rb
@@ -7,14 +7,15 @@ class RecipeFetcher::Adapters::NoMasDeMama
     # First, most common, search
     ingredients = document.search("//div[@class='meta']/p").map(&:text).
       select{|n| n =~ /\A\—/ }.map do |n|
-        n.gsub(/—/, '').strip
+        clean_ingredient(n)
       end
+
     if ingredients.empty?
       candidates = document.search("//div[@class='meta']/p")
       p = candidates.sort{|p2,p1| p1.to_html.length <=> p2.to_html.length}.first
 
       ingredients = p.to_html.split(/<br[^>]*>/)[2..-1].map do |n|
-        n.gsub(/—/, '').strip.gsub(/<[^>]+>/,'')
+        clean_ingredient(n)
       end
     end
 
@@ -22,9 +23,9 @@ class RecipeFetcher::Adapters::NoMasDeMama
   end
 
   def image
-    document.search("//div[@id='singlellarg']//img").map do |img|
+    if img = document.search("//div[@id='singlellarg']//img").first
       img.attributes['src'].value
-    end.first
+    end
   end
 
   def text
@@ -37,7 +38,7 @@ class RecipeFetcher::Adapters::NoMasDeMama
 
       html = node.to_html.strip
       if html.present?
-        text << html
+        text << "- #{clean_str(html)}"
       end
     end
 
@@ -52,5 +53,17 @@ class RecipeFetcher::Adapters::NoMasDeMama
 
   def document
     @document ||= Nokogiri.HTML(open(@url))
+  end
+
+  def clean_ingredient(str)
+    strip_tags(str.gsub(/\A\s—\s/, '').gsub(/\A\s*—\s*/, ''))
+  end
+
+  def clean_str(str)
+    strip_tags(str.gsub(/\d+\s–\s/, ''))
+  end
+
+  def strip_tags(str)
+    str.gsub(/<[^>]+>/,'').strip
   end
 end
--- spec/models/recipe_fetcher/adapters/no_mas_de_mama_spec.rb
+++ spec/models/recipe_fetcher/adapters/no_mas_de_mama_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe RecipeFetcher::Adapters::NoMasDeMama do
   context 'given a recipe of curry' do
     # This is the original URL
-    # let(:recipe_url) { 'http://www.nomas-demama.com/curry-rojo-vegetariano/' }
-    let(:recipe_url) { 'spec/support/recipes/www-nomas-demama-com-curry-rojo-vegetariano.html' }
+    # let(:recipe_url) { 'http://www.nomasdemama.com/curry-rojo-vegetariano/' }
+    let(:recipe_url) { 'spec/support/recipes/www-nomasdemama-com-curry-rojo-vegetariano.html' }
     let(:subject)    { RecipeFetcher::Adapters::NoMasDeMama.new(recipe_url) }
 
     it 'should be able to extract the ingredients' do
@@ -19,15 +19,44 @@ describe RecipeFetcher::Adapters::NoMasDeMama do
     end
 
     it 'should be able to extract the text' do
-      subject.text.should == "<p>Me ha costado dos semanas poder traerme a Marc y a Adrià a comer a casa. Sí, esa que estrené en julio. Sí, esa cuyos muebles compramos, transportamos y montamos nosotros —Hèctor, Alberto, Juanma y yo—. Eso, por cierto, nos tiene que otorgar por narices el derecho a guarrearlos y estropearlos. Otra cosa muy diferente —seamos racionales— es que vayamos a hacerlo. Pero el derecho está, nos lo hemos ganado. Ahí queda. Retomando el tema de Marc, Adrià y la famosa comida, sucedió ayer. Volví algo pronto del estudio, a eso de la una de la tarde, para preparar canelones rellenos de mató —pero esa es otra historia— y dejar un poco decente el salón. Lo que más me sorprendió fue que a Marc y Adrià les gustara (y bastante, según me dijeron. No, no es por compromiso. Si algo de lo que cocino no les va, no se ahorran la opinión) este <em>curry</em> rojo que serví de primero. Realmente, nunca lo había probado, por lo que no tenía muy claro cómo se supone que tiene que saber. Eso sí, yo me lo he comido por gula, por si se estropea o, haciendo gala de mi pesimismo, por si la raza humana se va a la mierda mañana. Esas cosas.</p>
-<p>1 – Calentamos una olla a fuego medio y rehogamos los ajos y los ajos tiernos picados en un chorro de aceite de girasol. Tras 1 minuto, agregamos la pasta de <em>curry</em> y sofreímos un par de minutos.</p>
-<p>2 – Agregamos 2 cucharadas de leche de coco y las zanahorias, peladas y cortadas en rodajas gruesas, de 5 mm de grosor. Rehogamos durante 6-7 minutos y añadimos las patatas, peladas y cortadas en láminas finas, la pastilla de caldo, el resto de la leche de coco y el agua. Cuando suba el hervor, dejamos cocer 5 minutos.</p>
-<p>3 – Escurrimos los garbanzos, los pasamos bajo el grifo y los incorporamos a la olla junto con los brotes de soja. Apagamos el fuego y dejamos reposar un par de minutos.</p>
-<p>4 – Servimos el <em>curry</em> en cuencos, añadimos un poco más de leche de coco, los brotes de alfalfa y un poco de lima.</p>"
+      subject.text.should == "- Me ha costado dos semanas poder traerme a Marc y a Adrià a comer a casa. Sí, esa que estrené en julio. Sí, esa cuyos muebles compramos, transportamos y montamos nosotros —Hèctor, Alberto, Juanma y yo—. Eso, por cierto, nos tiene que otorgar por narices el derecho a guarrearlos y estropearlos. Otra cosa muy diferente —seamos racionales— es que vayamos a hacerlo. Pero el derecho está, nos lo hemos ganado. Ahí queda. Retomando el tema de Marc, Adrià y la famosa comida, sucedió ayer. Volví algo pronto del estudio, a eso de la una de la tarde, para preparar canelones rellenos de mató —pero esa es otra historia— y dejar un poco decente el salón. Lo que más me sorprendió fue que a Marc y Adrià les gustara (y bastante, según me dijeron. No, no es por compromiso. Si algo de lo que cocino no les va, no se ahorran la opinión) este curry rojo que serví de primero. Realmente, nunca lo había probado, por lo que no tenía muy claro cómo se supone que tiene que saber. Eso sí, yo me lo he comido por gula, por si se estropea o, haciendo gala de mi pesimismo, por si la raza humana se va a la mierda mañana. Esas cosas.
+- Calentamos una olla a fuego medio y rehogamos los ajos y los ajos tiernos picados en un chorro de aceite de girasol. Tras 1 minuto, agregamos la pasta de curry y sofreímos un par de minutos.
+- Agregamos 2 cucharadas de leche de coco y las zanahorias, peladas y cortadas en rodajas gruesas, de 5 mm de grosor. Rehogamos durante 6-7 minutos y añadimos las patatas, peladas y cortadas en láminas finas, la pastilla de caldo, el resto de la leche de coco y el agua. Cuando suba el hervor, dejamos cocer 5 minutos.
+- Escurrimos los garbanzos, los pasamos bajo el grifo y los incorporamos a la olla junto con los brotes de soja. Apagamos el fuego y dejamos reposar un par de minutos.
+- Servimos el curry en cuencos, añadimos un poco más de leche de coco, los brotes de alfalfa y un poco de lima."
     end
 
     it 'should be able to extract a image' do
-      subject.image.should == 'http://www.nomas-demama.com/wp-content/uploads/2013/09/IMG_1740_1.jpg'
+      subject.image.should == 'http://www.nomasdemama.com/wp-content/uploads/2013/09/IMG_1740_1.jpg'
+    end
+  end
+
+  context 'given a recipe of potatoes and ribs' do
+    # This is the original URL
+    # let(:recipe_url) { 'http://www.nomasdemama.com/costillas-con-longaniza-y-patatas/' }
+    let(:recipe_url) { 'spec/support/recipes/www-nomasdemama-com-costillas-con-longaniza-y-patatas.html' }
+    let(:subject)    { RecipeFetcher::Adapters::NoMasDeMama.new(recipe_url) }
+
+    it 'should be able to extract the ingredients' do
+      subject.ingredients.should == ["200 g de longaniza a la pimienta",
+                                     "1 l de agua",
+                                     "1/2 vaso de vino blanco",
+                                     "3 patatas grandes",
+                                     "1 pimiento choricero",
+                                     "1 cebolla grande",
+                                     "1 tomate",
+                                     "2 dientes de ajo",
+                                     "1 rama de romero",
+                                     "Sal y pimienta",
+                                     "Aceite de oliva virgen extra"]
+    end
+
+    it 'should be able to extract the text' do
+      subject.text.should == "- Llevaba bastante tiempo queriendo hacer un guisote de estos pistonudos en los que carne y patatas se juntan. Como la longaniza catalana es de los pocos embutidos similares a las salchichas que me gustan, quería aprovechar la oportunidad de incorporarla para ver qué tal encajaba. He de decir que estoy muy contento con el resultado. No tanto por obra mía como por la calidad de los ingredientes, que era, sencillamente, altísima. A pesar de que todavía no ha llegado el frío, está bien ir desempolvando este tipo de recetas para que luego la rasca no nos pille oxidados. Eso sí, os recomiendo hidratar el pimiento choricero con antelación y luego aprovechar únicamente la pulpa en lugar de incorporarlo al guiso, como he hecho yo, ya que luego no tendréis que estar quitando trozos de piel, lo cual es (admitámoslo) un auténtico coñazo. Hala, salud.\n- Calentamos una olla a fuego medio-fuerte, añadimos un chorrito de aceite de oliva virgen extra y doramos en él las costillas y la longaniza, cortada en trozos de unos 4 cm. Salpimentamos.\n- Una vez que la carne haya tomado color por todos los lados, la retiramos y la reservamos. Bajamos el fuego a medio y sofreímos en el mismo aceite la cebolla y el ajo picados. Salamos ligeramente para que suelten el agua.\n- Pasados unos 10 minutos, añadimos el tomate rallado y dejamos que se evapore un poco el agua. Incorporamos la rama de romero, el pimiento choricero —al que habremos quitado el rabo y que habremos despepitado— y el vino blanco.\n- Cuando el vino haya perdido el alcohol, agregamos el agua y la carne que habíamos reservado. Subimos el fuego y, cuando empiece a hervir, lo bajamos de nuevo para que la cocción sea pausada. Probamos y corregimos de sal. Es importante que esté un poco fuerte de sabor, ya que luego las patatas lo rebajarán ligeramente. Dejamos cocer durante 45 minutos.\n- Pelamos las patatas y las cortamos chascándolas ligeramente, de tal manera que luego suelten almidón. Las añadimos al guiso y dejamos que cuezan durante 15-20 minutos o hasta que estén blandas. Por último, si queremos trabar un poco el guiso, trituramos un par de ellas y removemos bien."
+    end
+
+    it 'should be able to extract a image' do
+      subject.image.should == 'http://www.nomasdemama.com/blog/wp-content/uploads/2012/09/240-resultadolowres2.jpg'
     end
   end
 end
