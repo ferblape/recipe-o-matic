cb09dd86d9b49b4b3c3d55b660dd2eaa798cdb06
Fernando Blat <ferblape@gmail.com>
2013-12-09 20:50:26 +0100

    twitter cards

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,6 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
-//= require jquery.ui.autocomplete
 //= require turbolinks
 //= require_tree ./application
 //= require common_behaviours
