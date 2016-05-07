49bf907244b2a6c89d9f3c121cd846c8e7ec7ae4
Fernando Blat <ferblape@gmail.com>
2016-05-07 13:50:52 +0200

    New twitter credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'xxqWT6qTwJj6L6PxiUtxNg', 'sBkX9qx7pCJpfVbErEJ6ygPpY5RGJQECbM0BQykri0'
+  #provider :twitter, 'xxqWT6qTwJj6L6PxiUtxNg', 'sBkX9qx7pCJpfVbErEJ6ygPpY5RGJQECbM0BQykri0'
+  provider :twitter, 'L536Zxfv9prOjOnWTSLfOC2qK', 'HnIrC9TxB2qeM6m9qlIRXZKiQGIfyJwTt857ld2z1BY90AC96M'
 end
 
 OmniAuth.config.logger = Rails.logger
